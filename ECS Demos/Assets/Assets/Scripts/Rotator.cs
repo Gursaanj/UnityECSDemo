4c63f5d93609ae244b99889e82fc55599bef02fb
Gursaanj <gursaanjresides@gmail.com>
2019-09-07 22:08:34 -0700

    Completed First Demo Hybrid ECS
    Created First Demo, in which I use Hybrid ECS to Rotate A cube

---
+++ ECS Demos/Assets/Assets/Scripts/Rotator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+//Hybrid ECS:
+
+//Entity: this.gameobject
+
+//Component: variables manipulated by system, in Hybrid: Component can be derived from Monobehaviour
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    private float m_speed;
+
+    public float Speed
+    {
+        get
+        {
+            return m_speed;
+        }
+    }
+}
+
+//System: Using Logic and parameters of Component to work the given task
+public class RotatorSystem : ComponentSystem
+{
+    //To filter which components and Entities are worked on, create struct with specific components held only by one type of entity
+    private struct ComponentsDemo1
+    {
+        private Rotator m_rotator;
+        private Transform m_transform;
+
+        public Rotator Rotator
+        {
+            get
+            {
+                return m_rotator;
+            }
+        }
+
+        public Transform Transform
+        {
+            get
+            {
+                return m_transform;
+            }
+        }
+    }
+
+    //Using Unity Editor, you can convert GameObjects into Entities
+    protected override void OnUpdate()
+    {
+        float m_deltaTime = Time.deltaTime;
+
+        foreach (ComponentsDemo1 Entity in GetEntities<ComponentsDemo1>())
+        {
+            Entity.Transform.Rotate(0, Entity.Rotator.Speed*m_deltaTime, 0);
+        }
+    }
+}
